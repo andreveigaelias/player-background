d1a4030aba36e8ecae4e38b89fce092202aa7472
Admin <andreveigaelias@gmail.com>
2019-11-28 11:26:28 -0300

    vai

--- src/ios/PlayerBackground.swift
+++ src/ios/PlayerBackground.swift
@@ -1,26 +1,54 @@
-import AVFoundation
 import UIKit
+import AVFoundation
+import AVKit
 
 @objc(PlayerBackground) class PlayerBackground: CDVPlugin {
 
-     // This objc part is required above all plugin functions that you call from ionic.
-     // It allows us to communicate with Cordova from Swift.
+    var player = AVPlayer()
+    var playerViewcontroller = AVPlayerViewController()
+    
+
     @objc(startPlugin:)
-    func startPlugin(command: CDVInvokedUrlCommand) { // This is our swift function.
-        // Always set the plugin status as ERROR, until you do what you need to do.
+    func startPlugin(command: CDVInvokedUrlCommand) {
         var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR);
+        
+        print("Antes >>>>>>>>>>");
+                
+        guard let url = URL(string: "https://evpp.mm.uol.com.br/band_live/terraviva/playlist.m3u8") else {
+            return
+        }
+        
+        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+        player = AVPlayer(url: url)
+        
+        // Create a new AVPlayerViewController and pass it a reference to the player.
+        playerViewcontroller.player = player
+        playerViewcontroller.showsPlaybackControls = true
+        
+        
+        // Modally present the player and call the player's play() method when complete.
+        //present(playerViewcontroller, animated: true) {
+            self.player.play()
+        // }
 
-        // Plugin Command Arguments
-        // let x:Int = command.arguments![0] as! Int; // Change this accordingly.
-        // let y:Int = command.arguments![1] as! Int; // Change this accordingly.
-
-        // Whatever your plugin does after startup goes here.
-        print("PlayerBackground ran correctly!");
-        print("Thank you for using the Ionic-Plugin-Assistant");
-        print("Written by Jordan Benge @Bengejd");
+        print("Depois >>>>>>>>>>");
 
         // We did the stuff, and now we're finished. Set the status as OK.
         pluginResult = CDVPluginResult(status: CDVCommandStatus_OK);
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    }
+    
+    
+    //@objc(pause:)
+    public func pause() {
+        self.player.pause()
+    }
+    
+    public func disconnectAVPlayer() {
+        playerViewcontroller.player = nil
+    }
+   
+    public func reconnectAVPlayer() {
+        playerViewcontroller.player = player
     }
 }
--- www/PlayerBackground.js
+++ www/PlayerBackground.js
@@ -14,4 +14,14 @@ PlayerBackground.startPlugin = function(options, onSuccess, onError) {
 	exec(onSuccess, onError, PLUGIN_NAME, "startPlugin", [options.x, options.y]);
 };
 
+PlayerBackground.pause = function(options, onSuccess, onError) {
+	options = options || {};
+
+	// Here are the function options. It's recommended to set a default value, shown below.
+  options.x = options.x || 1;
+  options.y = options.y || 2;
+
+	exec(onSuccess, onError, PLUGIN_NAME, "pause", [options.x, options.y]);
+};
+
 module.exports = PlayerBackground;
